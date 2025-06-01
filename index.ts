2b2d27e2d331c33f6aa9a34b0b27ada6d522d9bd
Matti Jauhiainen <matti.tapio.jauhiainen@gmail.com>
2025-06-01 17:20:25 +0800

    Refactor project structure: Move JSON files to data directory and extract file list to fileIndex.js

--- index.ts
+++ index.ts
@@ -2,15 +2,15 @@ import { fetchActiveFlights, fetchFlightTrack } from "./fr24api.ts";
 
 async function getTrack(fr24_id: string) {
   const data = await fetchFlightTrack(fr24_id);
-  Deno.writeTextFile(`${fr24_id}.json`, JSON.stringify(data));
+  Deno.writeTextFile(`data/${fr24_id}.json`, JSON.stringify(data));
 }
 
-const ids = (await Deno.readTextFile("ids.txt")).split("\n");
-for (const id of ids.slice(180, 280)) {
-  console.log("Getting fr id: ", id);
-  await getTrack(id);
-  await new Promise((resolve) => setTimeout(resolve, 30_000));
-}
+// const ids = (await Deno.readTextFile("ids.txt")).split("\n");
+// for (const id of ids.slice(180, 280)) {
+//   console.log("Getting fr id: ", id);
+//   await getTrack(id);
+//   await new Promise((resolve) => setTimeout(resolve, 30_000));
+// }
 
 async function getFlightRadarIdsForTimestamp({
   timestamp,
@@ -22,6 +22,18 @@ async function getFlightRadarIdsForTimestamp({
   const data = await fetchActiveFlights(timestamp, airport);
   return new Set(data.data.map((flight) => flight.fr24_id));
 }
+
+// Read on of the id files and console log the lat-lon points of the path.
+// Put each lat-lon on its own line
+Deno.readTextFile("data/3930f673.json").then((data) => {
+  const flight = JSON.parse(data);
+  console.log(
+    flight[0].tracks.map((point) => `${point.lat}, ${point.lon}`).join("\n")
+  );
+  // for (const point of flight.data) {
+  //   console.log(`${point.latitude}, ${point.longitude}`);
+  // }
+});
 
 // let ids = new Set<string>();
 // for (let i = 0; i < 24; i++) {
